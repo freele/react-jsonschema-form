0cc523092f32cacb25096f6b2a10618a8f371b9e
Alexander Kremenets <kremenets0207@gmail.com>
2022-08-18 21:29:47 +0400

    wip

--- packages/playground/src/index.js
+++ packages/playground/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Theme4 as MuiV4Theme, Theme5 as MuiV5Theme } from "@rjsf/material-ui";
-import { Theme as FluentUITheme } from "@rjsf/fluent-ui";
-import { Theme as SuiTheme } from "@rjsf/semantic-ui";
-import { Theme as AntdTheme } from "@rjsf/antd";
+// import { Theme4 as MuiV4Theme, Theme5 as MuiV5Theme } from "@rjsf/material-ui";
+// import { Theme as FluentUITheme } from "@rjsf/fluent-ui";
+// import { Theme as SuiTheme } from "@rjsf/semantic-ui";
+// import { Theme as AntdTheme } from "@rjsf/antd";
 import { Theme as Bootstrap4Theme } from "@rjsf/bootstrap-4";
-import { Theme as ChakraUITheme } from "@rjsf/chakra-ui";
+// import { Theme as ChakraUITheme } from "@rjsf/chakra-ui";
 import { Theme as PrimeReactTheme } from "@rjsf/primereact";
 import Playground from "./app";
 import { render } from "react-dom";
@@ -89,37 +89,37 @@ const themes = {
       },
     },
   },
-  "material-ui-4": {
-    stylesheet: "",
-    theme: MuiV4Theme,
-  },
-  "material-ui-5": {
-    stylesheet: "",
-    theme: MuiV5Theme,
-  },
-  antd: {
-    stylesheet: "//cdnjs.cloudflare.com/ajax/libs/antd/4.1.4/antd.min.css",
-    theme: AntdTheme,
-  },
-  "semantic-ui": {
-    stylesheet:
-      "//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css",
-    theme: SuiTheme,
-  },
-  "fluent-ui": {
-    stylesheet:
-      "//static2.sharepointonline.com/files/fabric/office-ui-fabric-core/11.0.0/css/fabric.min.css",
-    theme: FluentUITheme,
-  },
+  // "material-ui-4": {
+  //   stylesheet: "",
+  //   theme: MuiV4Theme,
+  // },
+  // "material-ui-5": {
+  //   stylesheet: "",
+  //   theme: MuiV5Theme,
+  // },
+  // antd: {
+  //   stylesheet: "//cdnjs.cloudflare.com/ajax/libs/antd/4.1.4/antd.min.css",
+  //   theme: AntdTheme,
+  // },
+  // "semantic-ui": {
+  //   stylesheet:
+  //     "//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css",
+  //   theme: SuiTheme,
+  // },
+  // "fluent-ui": {
+  //   stylesheet:
+  //     "//static2.sharepointonline.com/files/fabric/office-ui-fabric-core/11.0.0/css/fabric.min.css",
+  //   theme: FluentUITheme,
+  // },
   "bootstrap-4": {
     stylesheet:
       "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
     theme: Bootstrap4Theme,
   },
-  "chakra-ui": {
-    stylesheet: "",
-    theme: ChakraUITheme,
-  },
+  // "chakra-ui": {
+  //   stylesheet: "",
+  //   theme: ChakraUITheme,
+  // },
   primereact: {
     stylesheets: [
       "https://unpkg.com/primeicons@5.0.0/primeicons.css",
